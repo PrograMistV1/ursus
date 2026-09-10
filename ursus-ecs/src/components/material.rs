60a360db599363306dcc588dbada54e51a359e67
PrograMistV1 <programist.v.1.1@gmail.com>
2026-09-10 22:54:31 +0200

    refactor: remove ursus-materials dependency and update material handling

--- ursus-ecs/src/components/material.rs
+++
@@ -1,8 +0,0 @@
-use crate::{Component, ComponentInit};
-use ursus_materials::MaterialHandle;
-
-impl Component for MaterialHandle {
-    fn check(_component: &mut Self, _builder: &hecs::EntityBuilder) {}
-}
-
-impl ComponentInit for MaterialHandle {}
--- ursus-ecs/src/components/mod.rs
+++ ursus-ecs/src/components/mod.rs
@@ -1,6 +1,5 @@
 pub mod camera;
 pub mod light;
-pub mod material;
 pub mod mesh;
 pub mod transform;
 pub mod transform_interpolation;
